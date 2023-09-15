e7d6e63e12d13a674aed58a12891a384da927087
cosmosunion <kosmosticlay@gmail.com>
2023-09-15 15:40:16 +0900

    #11.11
    bugs everywhere

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
-console.log();
-
 module.exports = {
-  entry: "./src/client/js/main.js",
+  entry: {
+    main: "./src/client/js/main.js",
+    videoPlayer: "./src/client/js/videoPlayer.js",
+  },
   mode: "development",
   watch: true,
   plugins: [
@@ -13,7 +14,7 @@ module.exports = {
     }),
   ],
   output: {
-    filename: "js/main.js",
+    filename: "js/[name].js",
     path: path.resolve(__dirname, "assets"),
     clean: true,
   },
@@ -29,7 +30,7 @@ module.exports = {
         },
       },
       {
-        test: /\.scss/,
+        test: /\.scss$/,
         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
     ],
