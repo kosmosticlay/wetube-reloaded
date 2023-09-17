10597e4e30a9c86ddcd8feb027e0db4ccc41b288
cosmosunion <kosmosticlay@gmail.com>
2023-09-17 20:17:18 +0900

    #13.2~13.5 Video Recorder updated

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,12 +1,50 @@
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
-const handleStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+let stream;
+let recorder;
+let videoFile;
+
+const handleDownload = () => {
+  const a = document.createElement("a");
+  a.href = videoFile;
+  // 해당 비디오 파일로 넘어가는 링크(<a>)를 생성
+  a.download = "MyRecording.webm";
+  // 포맷까지 지정하지 않을 경우 텍스트파일로 저장된다.
+  document.body.appendChild(a);
+  a.click();
+};
+
+const handleStop = () => {
+  startBtn.innerText = "Download Recording";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleDownload);
+  recorder.stop();
+};
+
+const handleStart = () => {
+  startBtn.innerText = "Stop Recording";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+  recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (event) => {
+    videoFile = URL.createObjectURL(event.data);
+    video.srcObject = null;
+    video.src = videoFile;
+    video.loop = true; // 반복재생 활성화
+    video.play();
+  };
+  recorder.start();
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
     audio: false,
     video: { width: 400, height: 200 },
   });
   video.srcObject = stream;
   video.play();
 };
+
+init(); // 버튼을 누르기 전에 preview가 보이도록 수정
 startBtn.addEventListener("click", handleStart);
