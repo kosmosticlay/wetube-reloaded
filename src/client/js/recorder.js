c28c61635de1e14743a33565fe968dbdb8e7757b
cosmosunion <kosmosticlay@gmail.com>
2023-09-18 17:19:46 +0900

    #14.3 Thumbnail implemented

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -32,16 +32,34 @@ const handleDownload = async () => {
 
   await ffmpeg.writeFile("recording.webm", await fetchFile(videoFile));
   await ffmpeg.exec(["-i", "recording.webm", "-r", "60", "output.mp4"]);
+  //
+  await ffmpeg.exec([
+    "-i",
+    "recording.webm",
+    "-ss",
+    "00:00:01",
+    "-frames:v",
+    "1",
+    "thumbnail.jpg",
+  ]);
 
-  const data = await ffmpeg.readFile("output.mp4");
-  const mp4Blob = new Blob([data.buffer], { type: "video/mp4" });
+  const mp4File = await ffmpeg.readFile("output.mp4");
+  const thumbFile = await ffmpeg.readFile("thumbnail.jpg");
+  const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" });
+  const thumBlob = new Blob([thumbFile.buffer], { type: "image/jpg" });
   const mp4Url = URL.createObjectURL(mp4Blob);
-
+  const thumbUrl = URL.createObjectURL(thumBlob);
   const a = document.createElement("a");
   a.href = mp4Url;
   a.download = "MyRecording.mp4";
   document.body.appendChild(a);
   a.click();
+
+  const thumbA = document.createElement("a");
+  thumbA.href = thumbUrl;
+  thumbA.download = "MyThumbnail.jpg";
+  document.body.appendChild(thumbA);
+  thumbA.click();
 };
 
 const handleStop = () => {
